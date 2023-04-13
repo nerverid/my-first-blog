8932c41ffc8072148bb023d4fa70985c4f570116
nerverid <nero-add@mail.ru>
2023-04-13 17:18:34 +0700

    Added urls and templates

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
 ]
