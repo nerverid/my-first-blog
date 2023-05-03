f7c9a28e7bd74d72c7b5c45fa8906e6e664195c4
nerverid <nero-add@mail.ru>
2023-05-03 17:27:21 +0700

    Added forms

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def post_list(request):
@@ -36,16 +37,27 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_url(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recomends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'nero.add@gmail.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return.render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 class PostListView(ListView):
     queryset = Post.published.all()
