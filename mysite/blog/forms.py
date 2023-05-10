e64342ba17d4271c8c00484c68845a074708a655
nerverid <nero-add@mail.ru>
2023-05-10 17:02:16 +0700

    Added comment system

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_lenght=25)
+    name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required = False,
